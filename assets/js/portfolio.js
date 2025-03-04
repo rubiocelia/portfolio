b2c6626b0bad984403b45850d98e1dfea9704684
Celia <rubiopaiscelia@gmail.com>
2025-03-04 14:17:05 +0100

    update 4/03

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,25 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var tabs = document.querySelectorAll(".menu-tab"); // Seleccionamos todas las pestañas
-  var galleryItems = document.querySelectorAll(".gallery__item"); // Seleccionamos todos los elementos de la galería
+  var tabs = document.querySelectorAll(".menu-tab");
+  var galleryItems = document.querySelectorAll(".gallery__item");
 
   tabs.forEach(function (tab) {
     tab.addEventListener("click", function (event) {
-      event.preventDefault(); // Evita el comportamiento por defecto del enlace
+      event.preventDefault();
 
-      var tabId = this.getAttribute("data-tab"); // Obtenemos el atributo data-tab de la pestaña seleccionada
+      var tabId = this.getAttribute("data-tab");
 
-      // Cambiar la clase activa en las pestañas
       tabs.forEach(function (t) {
         t.parentElement.classList.remove("portfolio__option--active");
       });
       this.parentElement.classList.add("portfolio__option--active");
 
-      // Mostrar u ocultar los elementos de la galería según la pestaña seleccionada
       galleryItems.forEach(function (item) {
         if (item.getAttribute("data-tab") === tabId || tabId === "all") {
-          item.classList.remove("hidden"); // Mostrar el item
+          item.classList.remove("hidden");
         } else {
-          item.classList.add("hidden"); // Ocultar el item
+          item.classList.add("hidden");
         }
       });
     });
